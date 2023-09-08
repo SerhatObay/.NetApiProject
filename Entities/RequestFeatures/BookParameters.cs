1ed2d7681d2b9a8d548b302538d8d7dc54755571
Serhat Obay <75859932+SerhatObay@users.noreply.github.com>
2023-09-08 15:18:06 +0300

    Searched Added

--- Entities/RequestFeatures/BookParameters.cs
+++ Entities/RequestFeatures/BookParameters.cs
@@ -1,9 +1,12 @@
-﻿namespace Entities.RequestFeatures
+﻿using System;
+
+namespace Entities.RequestFeatures
 {
     public class BookParameters : RequestParameters
     {
         public uint MinPrice { get; set; }
         public uint MaxPrice { get; set; } = 1000;
         public bool ValidPriceRange => MaxPrice > MinPrice;
+        public String? SearchTerm { get; set; }
     }
 }
